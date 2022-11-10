f5665acbcf934628d77a11f338514ab0ef0b91d9
Milan <milance.za019@gmail.com>
2022-11-10 15:19:13 +0100

    Dodavanje index stranice i njenog css-a

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Prijava doktorskog pregleda</title>
+
+</head>
+
+<body>
+    <div class="login-form">
+        <div class="main-div">
+            <form method="POST" action="#">
+                <div class="container">
+                    <label class="username">Korisnik</label>
+                    <input type="text" name="username" class="form-control" required>
+                    <br>
+                    <label for="password">Lozinka</label>
+                    <input type="password" name="password" class="form-control" required>
+                    <button type="submit" class="btn btn-primary" name="submit">Prijavite se</button>
+                </div>
+
+            </form>
+        </div>
+
+
+    </div>
+</body>
+
+</html>
